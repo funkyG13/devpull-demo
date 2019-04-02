50eced20878521c88fc2da354879ca130232971a
Mike T <m.tsiroglou13@gmail.com>
2019-04-02 14:13:46 +0300

    delete & update user

--- src/main/java/com/devpull/demo/controller/UserController.java
+++ src/main/java/com/devpull/demo/controller/UserController.java
@@ -2,8 +2,15 @@ package com.devpull.demo.controller;
 
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,19 +29,55 @@ public class UserController {
 		this.adminService = adminService;
 	}
 
+	//returns list of users at "/users"
 	@GetMapping("/users")
 	public List<User> users(){
 		return adminService.findAll();
 	}
 	
+	
 	@GetMapping("/users/{userId}")
-	public User selectUser(int id) {
-		return adminService.getUserById(id);
+	public User selectUser(@PathVariable int userId) {
+		User user =  adminService.getUserById(userId);
+		
+		if(user == null) {
+			throw new RuntimeException("User id not found: "+ userId);
+		}
+		return user;
 	}
 	
-	public void saveUser(User user) {
+	@PostMapping("/users")
+	public User addUser(@RequestBody User user) {
+		
+		//set id to 0 in order to force a save of a new item instead of update
+		user.setId(0);
+		
+		adminService.saveUser(user);;
+		 
+		return user;
+	}
+	
+	@PutMapping("/users")
+	public User updateUser(@RequestBody User user) {
 		
 		adminService.saveUser(user);
+		
+		return user;
+		
+	}
+	
+	@DeleteMapping("users/{userId}")
+	public String deleteUser(@PathVariable int userId) {
+		
+	User user = adminService.getUserById(userId);
+		
+	if(user == null) {
+		throw new RuntimeException( "The user with id "+ userId+ " doesn't exist.");
+	}
+	
+	adminService.deleteUserById(userId);
+	
+	return "User with id:"+ userId + " deleted";
 	}
 	
 }
